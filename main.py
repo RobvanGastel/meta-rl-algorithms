f8cd6dc5e8c64b8451a3f1aa25bcc27aef8b5188
Rob <robvangastel@hotmail.com>
2023-01-10 20:00:04 +0100

    Add meta-training to main.py, and add proper conda environment.yml

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yaml
 import argparse
 
 import torch
+import numpy as np
 from gymnasium.wrappers import RecordEpisodeStatistics
 
 from utils.logger import Logger
@@ -11,21 +12,30 @@ from envs.krazy_world.gym_wrapper import KrazyWorld
 
 
 def main(config):
-    Logger.get().info(f"Logger set. Initializing training {config['name']}")
+    Logger.get().info(f"Start meta-training RL2-PPO, {config['name']}")
 
     # Config
     ac_kwargs = {
-        "obs_enc_dim": 512,
-        "rnn_state_size": 128,
-        "actor_hidden_sizes": [128, 128],
-        "critic_hidden_sizes": [128, 128],
+        "obs_enc_dim": config["obs_enc_dim"],
+        "rnn_state_size": config["rnn_state_size"],
+        "actor_hidden_sizes": config["actor_hidden_sizes"],
+        "critic_hidden_sizes": config["critic_hidden_sizes"],
     }
+    device = torch.device(config["device"])
     ###
 
-    device = torch.device(config["device"])
+    # Distribution, same number of testing and training envs as used
+    # in E-MAML
+    envs = [
+        RecordEpisodeStatistics(KrazyWorld(seed=s, task_seed=s**2 + 1))
+        for s in range(32)
+    ]
+    test_envs = [
+        RecordEpisodeStatistics(KrazyWorld(seed=s, task_seed=s**2 + 1))
+        for s in range(100, 164)
+    ]
 
-    env = KrazyWorld(seed=20, task_seed=0)
-
+    env = np.random.choice(envs, 1)[0]
     agent = PPO(
         obs_dim=env.observation_space.shape[0],
         action_dim=env.action_space.n,
@@ -39,84 +49,102 @@ def main(config):
         device=device,
     )
 
-    # RL^2 variables
-    rnn_state = agent.actor_critic.initialize_state(batch_size=1)
-    prev_action = torch.tensor(0).to(device).unsqueeze(0)
-    prev_rew = torch.tensor(0).to(device).view(1, 1)
-    ###
-
-    # Number of episodes
     global_step = 0
-    for epoch in range(config["epochs"]):
-        obs, _ = env.reset()
-        ep_len, ep_rew = 0, 0
-        termination = False
+    for meta_epoch in range(config["meta_epochs"]):
 
-        while not termination:
-            obs = torch.tensor(obs).to(device).float().unsqueeze(0)
-            action, value, log_prob, rnn_state = agent.act(
-                obs, prev_action, prev_rew, rnn_state
-            )
-            next_obs, rew, termination, truncated, info = env.step(action)
+        # RL^2 variables
+        rnn_state = agent.actor_critic.initialize_state(batch_size=1)
+        prev_action = torch.tensor(0).to(device).unsqueeze(0)
+        prev_rew = torch.tensor(0).to(device).view(1, 1)
+        ###
 
-            buffer.store(
-                obs, action, rew, prev_action, prev_rew, termination, value, log_prob
-            )
+        # Sample new meta-training environment
+        env = np.random.choice(envs, 1)[0]
 
-            # Update the observation
-            obs = next_obs
+        # Iterate for number of episodes
+        for epoch in range(config["episodes"]):
+            termination, truncated = False, False
+            obs, _ = env.reset()
 
-            ep_len += 1
-            ep_rew += rew
-
-            # Set previous action and reward tensors
-            prev_action = action.detach()
-            prev_rew = torch.tensor(rew).to(device).view(1, 1)
-
-            if termination or truncated:
+            while not (termination or truncated):
                 obs = torch.tensor(obs).to(device).float().unsqueeze(0)
+                action, value, log_prob, rnn_state = agent.act(
+                    obs, prev_action, prev_rew, rnn_state
+                )
+                next_obs, rew, termination, truncated, info = env.step(action)
 
-                # The "value" argument should be 0 if the trajectory ended
-                # because the agent reached a terminal state (died).
-                if truncated:
-                    value = torch.tensor(0.0)
-                else:
-                    # Otherwise it should be V(s_t), the value function estimated for the
-                    # last state. This allows us to bootstrap the reward-to-go calculation
-                    # to account. for timesteps beyond the arbitrary episode horizon.
-                    _, value, _, _ = agent.act(obs, prev_action, prev_rew, rnn_state)
+                # termination
+                buffer.store(
+                    obs,
+                    action,
+                    rew,
+                    prev_action,
+                    prev_rew,
+                    termination,
+                    value,
+                    log_prob,
+                )
 
-                buffer.finish_path(value, termination)
+                # Update the observation
+                obs = next_obs
 
-                # Update every n episodes
-                if epoch % config["update_every_n"] == 0 and epoch != 0:
-                    batch, batch_size = buffer.get()
-                    print(f"update with batch size {batch_size}")
-                    agent.optimize(
-                        batch, config["update_epochs"], batch_size, global_step
-                    )
-                    buffer.reset()
+                # Set previous action and reward tensors
+                prev_action = action.detach()
+                prev_rew = torch.tensor(rew).to(device).view(1, 1)
 
-                    # f"time elapsed: {info['episode']['t']} "
-                    print(
-                        f"epoch/episode #: {epoch} "
-                        f"episode return: {ep_rew} "
-                        f" and episode length: {ep_len}",
-                        flush=True,
-                    )
-            global_step += 1
+                # if termination or truncated:
+                if termination or truncated:
+                    obs = torch.tensor(obs).to(device).float().unsqueeze(0)
+
+                    # The "value" argument should be 0 if the trajectory ended
+                    # because the agent reached a terminal state (died).
+                    if truncated:
+                        value = torch.tensor(0.0)
+                    else:
+                        # Otherwise it should be V(s_t), the value function estimated for the
+                        # last state. This allows us to bootstrap the reward-to-go calculation
+                        # to account. for timesteps beyond the arbitrary episode horizon.
+                        _, value, _, _ = agent.act(
+                            obs, prev_action, prev_rew, rnn_state
+                        )
+
+                    buffer.finish_path(value, termination)
+
+                    # Update every n episodes
+                    if epoch % config["update_every_n"] == 0 and epoch != 0:
+                        batch, batch_size = buffer.get()
+                        agent.optimize(
+                            batch, config["update_epochs"], batch_size, global_step
+                        )
+                        buffer.reset()
+
+                        Logger.get().info(
+                            f"meta-epoch: {meta_epoch} episode #: {epoch} "
+                            f"time elapsed: {info['episode']['t']} "
+                            f"episode return: {info['episode']['r']} "
+                            f" and episode length: {info['episode']['l']}"
+                        )
+
+                    # Log final episode reward
+                    writer = Logger.get().writer
+                    writer.add_scalar("PPO/return", info["episode"]["r"], global_step)
+
+                global_step += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", type=str)
-    parser.add_argument("-c", "--config", type=str, default="configs/rl2_ppo.yaml")
+    parser.add_argument("-c", "--config", type=str, default="configs/rl2_ppo.yml")
     args = parser.parse_args()
 
     with open(args.config, "r", encoding="utf-8") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
+
+    assert args.name is not None, "Pass a name for the experiment"
     config["name"] = args.name
+
     # Initialize logger
-    Logger(args.name, "logs")
+    Logger(args.name, f"runs/{args.name}")
 
     main(config)
